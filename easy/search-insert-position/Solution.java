8683d5c697089fb39e74ed5ad050cf65d9334166
Inforable <hasri.fayadh@gmail.com>
2026-04-09 22:35:29 +0700

    feat: solve find-the-index-of-the-first-occurence-in-a-string and search-insert-position

---
+++ easy/search-insert-position/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        int mid;
+
+        while (left < right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else {
+                if (target < nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            }
+        }
+        return left;
+    }
+}
