a44f9593231fcb719be32003535e2b4930573d7b
Inforable <hasri.fayadh@gmail.com>
2026-04-13 08:43:27 +0700

    feat: solve minimum-depth-of-binary-tree, pascals-triangle, and path-sum

---
+++ easy/pascals-triangle/Solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            if (i == 0) {
+                row.add(1);
+            } else if (i == 1) {
+                row.add(1);
+                row.add(1);
+            } else {
+                List<Integer> prevRow = result.get(i - 1);
+                row.add(1);
+                for (int j = 1; j < prevRow.size(); j++) {
+                    row.add(prevRow.get(j - 1) + prevRow.get(j));
+                }
+                row.add(1);
+            }
+            result.add(row);
+        }
+        return result;
+    }
+}
